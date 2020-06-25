e8f3b8426965cc38896325d812e4e8e89e2cc6ab
ThatGeekyWeeb <ssfgames13@gmail.com>
2020-06-25 12:56:26 -0400

    finished ./install.sh ~ Require xorg env

--- install.sh
+++ install.sh
@@ -9,6 +9,23 @@ Which of the following are fruits?" 30 70 3 \
         "TimeType"   "'Time='Now'" off \
         "None"       "Basic Install" off 2> /tmp/checklist.tmp.$$
 
+timetype1() {
+	touch -a "$HOME/.config/fish/config.fish"
+	echo 'export timetype="true"' >> "$HOME/.config/fish/config.fish"
+}
+
+battery1() {
+        touch -a "$HOME/.config/fish/config.fish"
+        echo 'export bat="true"' >> "$HOME/.config/fish/config.fish"
+        curl -O "https://raw.githubusercontent.com/holman/spark/master/spark"
+       	sudo mv spark "/usr/local/bin"
+        sudo chmod u+x "/usr/local/bin/spark"
+	curl -O "https://raw.githubusercontent.com/goles/battery/master/battery"
+	sudo mv battery "/usr/local/bin"
+	sudo chmod u+x "/usr/local/bin/battery"
+}
+
+
 retval=$?
 
 choice=`cat /tmp/checklist.tmp.$$`
@@ -25,29 +42,6 @@ esac
 var1=$(echo "$choice")
 choice=""
 echo "$var1"
-./Xdialog --title "Config" \
-        --msgbox "Hello!
-        In the next section select
-        Your 'config.fish' location!" 10 41
-case $? in
-  0)
-    echo "OK";;
-  255)
-    echo "Box closed."
-    exit 1;;
-esac
-FILE=`./Xdialog --title "Please choose a file" --fselect /home 32 60 2>&1`
-
-case $? in
-	0)
-		var21=$(echo "$FILE");;
-	1)
-		exit 1;;
-	255)
-		exit 1;;
-esac
-var112=$(echo "$var21" | grep "config.fish")
-grep1=$(echo "$var1" | grep -q "Battery")
 case $? in
     	0)
         	./Xdialog --title "Installing Battery" --yesno "Installing 'Battery'
@@ -55,11 +49,9 @@ case $? in
 		case $? in
     			0)
         			echo "sudo allowed"
-				echo "installing 'Battery'"
-				curl -O "https://raw.githubusercontent.com/holman/spark/master/spark"
-				sudo mv "./spark" "/usr/local/bin";;
+				echo "installing 'Battery'";;
         		1)
-            			echo "$var1";;
+            			var1=$(echo "$var1" | cut -d/ -f2);;
         		255)
             			exit 1;;
 		esac
@@ -67,23 +59,19 @@ case $? in
 	1)
     	exit 1;;
 esac
-
-./Xdialog --title Prompt Location --msgbox "Select Prompt loaction" 40 40
-case $? in
-	0)
-    		FILE=`./Xdialog --title "Please choose a file" --fselect /home 32 60 2>&1`
-		case $? in
-			0)
-				loc=$(echo "$FILE");;
-			1)
-				exit 1;;
-			255)
-				exit 1;;
-		esac
-		;;
-esac
 do_install() {
-    install -Dm 644 ./SquidPrompt.fish "$loc"
+    install -Dm 644 ./SquidPrompt.fish "$HOME/.config/fish/functions/fish_prompt.fish"
 }
 do_install
-exit 0
+case "$var1" in
+    	Battery)
+        	battery1
+            	exit 0;;
+        TimeType)
+            	timetype1
+            	exit 0;;
+	Battery/TimeType)
+    		battery1
+    		timetype1
+    		exit 0;;
+esac
